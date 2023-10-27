2c52374ae8cdbfca88401469765151b8b2fd8529
Hampus Åström <hampus.thep@gmail.com>
2023-10-27 16:27:05 +0200

    Broke up docker run script for readability

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 SHARED_FOLDER=$(dirname $(readlink -f $0) | rev | cut -d'/' -f1- | rev)
 
-docker run -it --runtime=nvidia --rm --network host --ipc=host --name test --user=$( id -u $USER ):$( id -g $USER ) --volume="/etc/group:/etc/group:ro" --volume="/etc/passwd:/etc/passwd:ro" --volume="/etc/shadow:/etc/shadow:ro" --volume="/etc/sudoers.d:/etc/sudoers.d:ro" -v ${SHARED_FOLDER}:/shared-folder -e DISPLAY=$DISPLAY --env QT_X11_NO_MITSHM=1 --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --mount src="$(pwd)",target=/home/mamba/stable-baselines3,type=bind stablebaselines/stable-baselines3 bash
+docker run -it --runtime=nvidia --rm --network host --ipc=host \
+    --privileged \
+    --name test \
+    --user=$( id -u $USER ):$( id -g $USER ) \
+    --volume="/etc/group:/etc/group:ro" \
+    --volume="/etc/passwd:/etc/passwd:ro" \
+    --volume="/etc/shadow:/etc/shadow:ro" \
+    --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+    -v ${SHARED_FOLDER}:/shared-folder \
+    -e DISPLAY=$DISPLAY \
+    --env QT_X11_NO_MITSHM=1 \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --mount src="$(pwd)",target=/home/mamba/stable-baselines3,type=bind stablebaselines/stable-baselines3 bash
