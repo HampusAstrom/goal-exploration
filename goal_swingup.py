56554d6592ffecf8d983b9899dc0ddf98ae18849
Hampus Åström <hampus.thep@gmail.com>
2023-11-06 21:13:54 +0100

    Make method of goal_swingup and add some versions, also automate more in plot

--- goal_swingup.py
+++ goal_swingup.py
@@ -1,6 +1,7 @@
 import os
 import gymnasium as gym
 import numpy as np
+from itertools import product
 
 from stable_baselines3 import SAC, HerReplayBuffer
 from stable_baselines3.common.callbacks import EvalCallback
@@ -12,106 +13,108 @@ import imageio
 
 from goal_pendulum import GoalPendulumEnv
 
-env_id = "GoalPendulumEnv-v0"
-n_training_envs = 1
-n_eval_envs = 5
+def train(base_path: str = "./output/pendulum/hardstart/",
+         fixed_eval: bool = True,
+         goal: str = "random",
+         density: str = "dense",
+         reward_type: str = "pendulum",
+         steps: int = 10000,
+         experiment: str = "exp1",
+         harder_start: float = 0.2,
+         ):
 
-# Create 4 artificial transitions per real transition
-n_sampled_goal = 4
+    env_id = "GoalPendulumEnv-v0"
+    n_training_envs = 1
+    n_eval_envs = 5
 
-# TODO replace with command line argument
-base_path = "./temp/pendulum/"
-fixed_eval = True
-if fixed_eval:
-    base_path = os.path.join(base_path, "dense_pendulum_eval")
-goal = "random"
-density = "dense"
-reward_type = "pendulum"
-steps = 10000
-options = goal + "_" + density + "_" + reward_type + "_" + str(steps)
-experiment = "exp1"
-harder_start = 0.2
+    # Create 4 artificial transitions per real transition
+    n_sampled_goal = 4
 
-# Create log dir
-log_dir = os.path.join(base_path, options, experiment, "train_logs")
-os.makedirs(log_dir, exist_ok=True)
+    # TODO replace with command line argument
+    if fixed_eval:
+        base_path = os.path.join(base_path, "truncated_pendulum_eval")
+    options = goal + "_" + density + "_" + reward_type + "_" + str(steps) + "steps_" + str(harder_start) + "hardstart"
 
-# Initialize a vectorized training environment with default parameters
-#train_env = make_vec_env(env_id, n_envs=n_training_envs, seed=0)
-#train_env = GoalPendulumEnv(render_mode="human")
-if goal == "fixed":
-    train_env = gym.make(env_id,
-                         reward_type=reward_type,
-                         reward_density=density,
-                         fixed_goal=np.array([1.0, 0.0, 0.0]),
-                         harder_start=harder_start,
-                         )
-else:
-    train_env = gym.make(env_id,
-                         reward_type=reward_type,
-                         reward_density=density,
-                         harder_start=harder_start,)
-train_env = Monitor(train_env, log_dir)
+    # Create log dir
+    log_dir = os.path.join(base_path, options, experiment, "train_logs")
+    os.makedirs(log_dir, exist_ok=True)
 
-# Create log dir where evaluation results will be saved
-eval_log_dir = os.path.join(base_path, options, experiment, "eval_logs")
-os.makedirs(eval_log_dir, exist_ok=True)
+    # Initialize a vectorized training environment with default parameters
+    #train_env = make_vec_env(env_id, n_envs=n_training_envs, seed=0)
+    #train_env = GoalPendulumEnv(render_mode="human")
+    if goal == "fixed":
+        train_env = gym.make(env_id,
+                            reward_type=reward_type,
+                            reward_density=density,
+                            fixed_goal=np.array([1.0, 0.0, 0.0]),
+                            harder_start=harder_start,
+                            )
+    else:
+        train_env = gym.make(env_id,
+                            reward_type=reward_type,
+                            reward_density=density,
+                            harder_start=harder_start,)
+    train_env = Monitor(train_env, log_dir)
 
-# Separate evaluation env, with different parameters passed via env_kwargs
-# Eval environments can be vectorized to speed up evaluation.
-#eval_env = make_vec_env(env_id, n_envs=n_training_envs, seed=0)
-#eval_env = GoalPendulumEnv(render_mode="human",
-#                           fixed_goal=np.array([1.0, 0.0, 0.0]))
-if fixed_eval:
-    eval_env = gym.make(env_id,
-                        render_mode="human",
-                        fixed_goal=np.array([1.0, 0.0, 0.0]),
-                        harder_start=harder_start,
-                        reward_type="pendulum",
-                        reward_density="dense")
-else:
-    eval_env = gym.make(env_id,
-                        render_mode="human",
-                        fixed_goal=np.array([1.0, 0.0, 0.0]),
-                        harder_start=harder_start,
-                        reward_type=reward_type,
-                        reward_density=density)
-eval_env = Monitor(eval_env, eval_log_dir)
+    # Create log dir where evaluation results will be saved
+    eval_log_dir = os.path.join(base_path, options, experiment, "eval_logs")
+    os.makedirs(eval_log_dir, exist_ok=True)
 
-# Create callback that evaluates agent for 5 episodes every 500 training environment steps.
-# When using multiple training environments, agent will be evaluated every
-# eval_freq calls to train_env.step(), thus it will be evaluated every
-# (eval_freq * n_envs) training steps. See EvalCallback doc for more information.
-eval_callback = EvalCallback(eval_env, 
-                             best_model_save_path=eval_log_dir,
-                             log_path=eval_log_dir,
-                             eval_freq=max(500 // n_training_envs, 1),
-                             n_eval_episodes=5,
-                             deterministic=True,
-                             render=True)
+    # Separate evaluation env, with different parameters passed via env_kwargs
+    # Eval environments can be vectorized to speed up evaluation.
+    #eval_env = make_vec_env(env_id, n_envs=n_training_envs, seed=0)
+    #eval_env = GoalPendulumEnv(render_mode="human",
+    #                           fixed_goal=np.array([1.0, 0.0, 0.0]))
+    if fixed_eval:
+        eval_env = gym.make(env_id,
+                            render_mode="human",
+                            fixed_goal=np.array([1.0, 0.0, 0.0]),
+                            harder_start=harder_start,
+                            reward_type="pendulum",
+                            reward_density="truncated")
+    else:
+        eval_env = gym.make(env_id,
+                            render_mode="human",
+                            fixed_goal=np.array([1.0, 0.0, 0.0]),
+                            harder_start=harder_start,
+                            reward_type=reward_type,
+                            reward_density=density)
+    eval_env = Monitor(eval_env, eval_log_dir)
 
-#check_env(train_env)
+    # Create callback that evaluates agent for 5 episodes every 500 training environment steps.
+    # When using multiple training environments, agent will be evaluated every
+    # eval_freq calls to train_env.step(), thus it will be evaluated every
+    # (eval_freq * n_envs) training steps. See EvalCallback doc for more information.
+    eval_callback = EvalCallback(eval_env,
+                                best_model_save_path=eval_log_dir,
+                                log_path=eval_log_dir,
+                                eval_freq=max(500 // n_training_envs, 1),
+                                n_eval_episodes=5,
+                                deterministic=True,
+                                render=True)
 
-model = SAC("MultiInputPolicy",
-            train_env,
-            replay_buffer_class=HerReplayBuffer,
-            replay_buffer_kwargs=dict(
-                n_sampled_goal=n_sampled_goal,
-                goal_selection_strategy="future",
-                copy_info_dict=True,
-            ),
-            learning_starts=300,
-            verbose=1,
-            buffer_size=int(1e6),
-            learning_rate=1e-3,
-            gamma=0.95,
-            batch_size=256,
-            policy_kwargs=dict(net_arch=[256, 256, 256]),
-)
-model.learn(steps, callback=eval_callback, progress_bar=True)
-#model.learn(5000, progress_bar=True)
-model_path = os.path.join(base_path, options, experiment, 'model')
-model.save(model_path)
+    #check_env(train_env)
+
+    model = SAC("MultiInputPolicy",
+                train_env,
+                replay_buffer_class=HerReplayBuffer,
+                replay_buffer_kwargs=dict(
+                    n_sampled_goal=n_sampled_goal,
+                    goal_selection_strategy="future",
+                    copy_info_dict=True,
+                ),
+                learning_starts=300,
+                verbose=1,
+                buffer_size=int(1e6),
+                learning_rate=1e-3,
+                gamma=0.95,
+                batch_size=256,
+                policy_kwargs=dict(net_arch=[256, 256, 256]),
+    )
+    model.learn(steps, callback=eval_callback, progress_bar=True)
+    #model.learn(5000, progress_bar=True)
+    model_path = os.path.join(base_path, options, experiment, 'model')
+    model.save(model_path)
 
 #model.load(model_path, eval_env)
 
@@ -155,3 +158,18 @@ model.save(model_path)
 #     print(f"Max: {np.max(res)}")
 
 #     print(np.sort(res))
+
+if __name__ == '__main__':
+    goal = ["fixed", "random",]
+    density = ["truncated"] # ["truncated", "dense"]
+    reward_type = ["pendulum"] # ["pendulum","genericnormed"]
+    harder_start = [0.1, 0.2, 0.5, 1]
+    
+    for conf in product(goal, density, reward_type, harder_start):
+        train(goal=conf[0],
+              density=conf[1],
+              reward_type=conf[2],
+              harder_start=conf[3],
+              experiment="exp3",
+              )
+        print(conf[0]+conf[1]+conf[2]+str(conf[3]))
--- plot.py
+++ plot.py
@@ -2,77 +2,91 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-paths = ["./output/pendulum/trunc_pendulum_eval/fixed_dense_genericnormed_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/fixed_dense_pendulum_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/fixed_sparse_genericnormed_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/fixed_sparse_pendulum_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/fixed_truncated_genericnormed_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/fixed_truncated_pendulum_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/random_dense_genericnormed_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/random_dense_pendulum_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/random_sparse_genericnormed_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/random_sparse_pendulum_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/random_truncated_genericnormed_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/random_truncated_pendulum_10000/exp1/eval_logs",
-        ]
-labels = ["fixed_dense_genericnormed_10000",
-          "fixed_dense_pendulum_10000",
-          "fixed_sparse_genericnormed_10000",
-          "fixed_sparse_pendulum_10000",
-          "fixed_truncated_genericnormed_10000",
-          "fixed_truncated_pendulum_10000",
-          "random_dense_genericnormed_10000",
-          "random_dense_pendulum_10000",
-          "random_sparse_genericnormed_10000",
-          "random_sparse_pendulum_10000",
-          "random_truncated_genericnormed_10000",
-          "random_truncated_pendulum_10000",]
-
 # current selection
-paths = ["./output/pendulum/trunc_pendulum_eval/fixed_dense_genericnormed_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/fixed_dense_pendulum_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/fixed_truncated_genericnormed_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/fixed_truncated_pendulum_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/random_dense_genericnormed_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/random_dense_pendulum_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/random_truncated_genericnormed_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/random_truncated_pendulum_10000/exp1/eval_logs",
-        ]
-labels = ["fixed_dense_genericnormed_10000",
-          "fixed_dense_pendulum_10000",
-          "fixed_truncated_genericnormed_10000",
-          "fixed_truncated_pendulum_10000",
-          "random_dense_genericnormed_10000",
-          "random_dense_pendulum_10000",
-          "random_truncated_genericnormed_10000",
-          "random_truncated_pendulum_10000",]
+# paths = ["./output/pendulum/trunc_pendulum_eval/fixed_dense_genericnormed_10000/exp1/eval_logs",
+#         "./output/pendulum/trunc_pendulum_eval/fixed_dense_pendulum_10000/exp1/eval_logs",
+#         "./output/pendulum/trunc_pendulum_eval/fixed_truncated_genericnormed_10000/exp1/eval_logs",
+#         "./output/pendulum/trunc_pendulum_eval/fixed_truncated_pendulum_10000/exp1/eval_logs",
+#         "./output/pendulum/trunc_pendulum_eval/random_dense_genericnormed_10000/exp1/eval_logs",
+#         "./output/pendulum/trunc_pendulum_eval/random_dense_pendulum_10000/exp1/eval_logs",
+#         "./output/pendulum/trunc_pendulum_eval/random_truncated_genericnormed_10000/exp1/eval_logs",
+#         "./output/pendulum/trunc_pendulum_eval/random_truncated_pendulum_10000/exp1/eval_logs",
+#         ]
+# labels = ["fixed_dense_genericnormed_10000",
+#           "fixed_dense_pendulum_10000",
+#           "fixed_truncated_genericnormed_10000",
+#           "fixed_truncated_pendulum_10000",
+#           "random_dense_genericnormed_10000",
+#           "random_dense_pendulum_10000",
+#           "random_truncated_genericnormed_10000",
+#           "random_truncated_pendulum_10000",]
 
-linestyles = ["r-",
-             "r--",
-             "r:",
-             "r-.",
-             "b-",
-             "b--",
-             "b:",
-             "b-.",
-             ]
+# linestyles = ["r-",
+#              "r--",
+#              "r:",
+#              "r-.",
+#              "b-",
+#              "b--",
+#              "b:",
+#              "b-.",
+#              ]
+
+from itertools import product
+
+goal = ["fixed", "random",]
+density = ["truncated"] # ["truncated", "dense"]
+reward_type = ["pendulum"] # ["pendulum","genericnormed"]
+harder_start = [0.1, 0.2, 0.5, 1] # [0.1, 0.2, 0.5, 1]
+experiments = ["exp1", "exp2", "exp3"]
+
+lineparts = product(["-", ":", "-", "-."], ["r", "g", "c", "b"]) # ["--", ":", "-", "-."], ["r", "y", "g", "b"]
+colors = ["r", "g", "c", "b"]*4
+linestyles = [x[0]+x[1] for x in lineparts]
+skips = [0, 0, 1, 0, 0, 0, 1, 0]
+#skips = [x == 1 for x in skips]
+
+print(colors)
+print(linestyles)
+
+paths = []
+labels = []
+base_path = "./output/pendulum/hardstart/truncated_pendulum_eval/"
+for conf in product(goal, density, reward_type, harder_start):
+    (goals, densitys, reward_types, harder_starts)=conf
+    steps = 20000
+    # experiment = "exp1"
+    exp_set = []
+    options = goals + "_" + densitys + "_" + reward_types + "_" + str(steps) + "steps_" + str(harder_starts) + "hardstart"
+    short_options = str(harder_starts) + " easyness " + goals + " goal"
+    for experiment in experiments:
+        eval_log_dir = os.path.join(base_path, options, experiment, "eval_logs")
+        exp_set.append(eval_log_dir)
+    paths.append(exp_set)
+    #labels.append(options)
+    labels.append(short_options)
+
 
 px = 1/plt.rcParams['figure.dpi']
 fig, ax = plt.subplots(figsize=(1920*px, 1080*px))
 window = 10
-for path, label, linestyle in zip(paths, labels, linestyles):
-    data = np.loadtxt(os.path.join(path, "monitor.csv"), delimiter=',', skiprows=2, usecols=0)
-    avg_data = np.convolve(data, [1]*window, 'valid')
+for path, label, linestyle, color, skip in zip(paths, labels, linestyles, colors, skips):
+    if skip:
+        continue
+    datas = []
+    for exp in path:
+        data = np.loadtxt(os.path.join(exp, "monitor.csv"), delimiter=',', skiprows=2, usecols=0)
+        #avg_data = np.convolve(data, [1]*window, 'valid')/window
+        datas.append(data)
+    #avg_data = np.mean(datas, axis=0)
+    #avg_std = np.std(datas, axis=0)
+    data = np.mean(datas, axis=0)
+    std = np.std(datas, axis=0)
+    avg_data = np.convolve(data, [1]*window, 'valid')/window
+    avg_std = np.convolve(std, [1]*window, 'valid')/window
     x = range(len(avg_data))
     ax.plot(x, avg_data, linestyle, label=label)
+    ax.fill_between(x, avg_data+avg_std, avg_data-avg_std, alpha=0.03, facecolor=color)
 
 ax.legend()
-plt.savefig("test_plot")
-
-paths = ["./output/pendulum/dense_pendulum_eval/random_dense_pendulum_10000/exp1/eval_logs",
-        "./output/pendulum/trunc_pendulum_eval/fixed_dense_pendulum_10000/exp1/eval_logs",
-        ]
-
-labels = ["random_dense_pendulum_10000",
-          "fixed_dense_pendulum_10000",
-          ]
+fig.tight_layout()
+plt.savefig("compare_goal_trunc_pen_trunc_eval_test")
