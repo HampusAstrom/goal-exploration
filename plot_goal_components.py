6027ecb774b940a17d4dd1a37dffa7e3a7f65f43
Hampus Åström <hampus.thep@gmail.com>
2024-03-24 18:36:40 +0100

    Started making code to analyze and plot goal components from saved data

---
+++ plot_goal_components.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+with open('/shared-folder/temp/wrapper/SparsePendulumEnv-v1/20000steps_1.0goalrewardWeight_0.0fixedGoalFraction_[1,1,5,1,20]component_weights/test1/eval_logs/goal_component_map.txt', newline='') as csvfile:
+
+    reader = csv.reader(filter(lambda row: row[0]!='#', csvfile),
+                        delimiter=' ',
+                        quoting=csv.QUOTE_NONNUMERIC)
+
+    data = np.array(list(reader))
+
+    print("mean values of each column:")
+    means = np.mean(data, 0)
+    maxes = np.max(data, 0)
+    np.set_printoptions(precision=5, suppress=True)
+    print(means)
+    print("max values of each column:")
+    print(maxes)
