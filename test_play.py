f5211f0e3e8cca9b5133ba61b32d0d5476622d5a
Hampus Åström <hampus.thep@gmail.com>
2025-01-20 15:23:41 +0100

    Corrected play method to right env version

--- test_play.py
+++ test_play.py
@@ -3,7 +3,7 @@ from gymnasium.utils import play
 
 from pathological_mc import PathologicalMountainCarEnv
 
-env = play.play(gym.make('PathologicalMountainCar-v1', render_mode='rgb_array', terminate=True).env,
+env = play.play(gym.make('PathologicalMountainCar-v1.1', render_mode='rgb_array', terminate=True).env,
                 zoom=1,
                 keys_to_action={"1":0, "a":0, "2":2, "d":2,},
                 noop=1)
